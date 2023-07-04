c56431d0fcf49710513dedc04a03dc10a1554f1e
spierala <mail@florian-spier.de>
2023-07-04 21:10:16 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <app-root></app-root>
   `,
 })
 export class App {
