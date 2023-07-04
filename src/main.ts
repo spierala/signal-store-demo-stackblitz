35e5b971a1edb18888ed2df2c53e38ccd26f03a7
spierala <mail@florian-spier.de>
2023-07-04 21:20:35 +0200

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,10 @@ import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-  <h1>Hello from {{name}}!</h1>
-  <a target="_blank" href="https://angular.io/start">
-    Learn more about Angular 
-  </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(App);
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+
